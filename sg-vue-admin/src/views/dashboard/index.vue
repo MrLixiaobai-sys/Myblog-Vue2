dfd43cbb13b9aa7917e722ef3e97f0c34295d4ab
pingYun01/shuYinLi <1461760901@qq.com>
2025-01-24 15:46:22 +0800

    first commit

---
+++ sg-vue-admin/src/views/dashboard/index.vue
@@ -0,0 +1,34 @@
+<template>
+  <div class="dashboard-container">
+    <div class="dashboard-text">
+      李书印博客管理系统
+    </div>
+    <div>
+      项目获取qq联系：1461760901。
+    </div>
+  </div>
+</template>
+
+<script>
+import { mapGetters } from 'vuex'
+export default {
+  name: 'Dashboard',
+  computed: {
+    ...mapGetters([
+      'name'
+    ])
+  }
+}
+</script>
+
+<style lang="scss" scoped>
+.dashboard {
+  &-container {
+    margin: 30px;
+  }
+  &-text {
+    font-size: 30px;
+    line-height: 46px;
+  }
+}
+</style>
